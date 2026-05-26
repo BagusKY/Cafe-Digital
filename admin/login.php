94607a0b4648b4c0feb37980ee6456aca0e0e2fe
BagusKY <167533407+BagusKY@users.noreply.github.com>
2026-05-27 00:42:35 +0700

    Add files via upload

---
+++ admin/login.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Admin Login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body class="bg-dark d-flex align-items-center justify-content-center" style="height:100vh;">
+
+<div class="card p-5" style="width:400px;">
+
+    <h2 class="text-center mb-4">ADMIN LOGIN</h2>
+
+    <form action="dashboard.php">
+
+        <input type="text" class="form-control mb-3" placeholder="Username">
+
+        <input type="password" class="form-control mb-3" placeholder="Password">
+
+        <button class="btn btn-dark w-100">
+            Login
+        </button>
+
+    </form>
+
+</div>
+
+</body>
+</html>
